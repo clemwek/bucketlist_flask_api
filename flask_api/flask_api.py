0241810a37bcf5166408769f60e478b19998b05f
Dhwty <dhwty@localhost.localdomain>
2017-07-25 10:42:15 +0300

    Add models

--- flask_api/flask_api.py
+++ flask_api/flask_api.py
@@ -7,8 +7,14 @@ from flask_api.models.bucketlist.views import bucketlist_blueprints
 
 
 app = Flask(__name__)
-app.config['SQLALCHEM_DATABASE_URI'] = 'postgressql://postgres:newpassword@localhost/flask_api_db'
+app.config.from_object('config')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+
+from flask_api.models.users.user import User
+from flask_api.models.bucketlist.bucketlist import Bucketlist
+from flask_api.models.items.items import Item
 
 app.register_blueprint(user_blueprints, url_prefix='/auth')
 app.register_blueprint(bucketlist_blueprints)
