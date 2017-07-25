96ffe63e90d132677f93c2ec1e60511bf4b7fe21
Dhwty <dhwty@localhost.localdomain>
2017-07-25 10:42:33 +0300

    Update Config

---
+++ config.py
@@ -0,0 +1,20 @@
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'thisissecret'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:newpassword@localhost/flask_api_db'
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:newpassword@localhost/test_flask_api'
