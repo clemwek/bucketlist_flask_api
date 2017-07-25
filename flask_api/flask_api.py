28f4669020a731608163724b7bba27f202fbcb68
Dhwty <dhwty@localhost.localdomain>
2017-07-25 04:36:31 +0300

    Setup database

--- flask_api/flask_api.py
+++ flask_api/flask_api.py
@@ -1,4 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+from flask_api.models.users.views import user_blueprints
+from flask_api.models.bucketlist.views import bucketlist_blueprints
 
 
 app = Flask(__name__)
+app.config['SQLALCHEM_DATABASE_URI'] = 'postgressql://postgres:newpassword@localhost/flask_api_db'
+db = SQLAlchemy(app)
+
+app.register_blueprint(user_blueprints, url_prefix='/auth')
+app.register_blueprint(bucketlist_blueprints)
