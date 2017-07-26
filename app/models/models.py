d07f0e6f11aba82b6964bfe9cdbfbbb8d83ae8f6
Dhwty <dhwty@localhost.localdomain>
2017-07-26 22:57:47 +0300

    Removed the name colum

--- app/models/models.py
+++ app/models/models.py
@@ -7,7 +7,6 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
     username = db.Column(db.String(), unique=True)
     email = db.Column(db.String(), unique=True)
     password = db.Column(db.String())
@@ -15,7 +14,6 @@ class User(db.Model):
 
     def __init__(self, name, username, email, password):
         """initialize with name, username, email, password."""
-        self.name = name
         self.username = username
         self.email = email
         self.password = password
