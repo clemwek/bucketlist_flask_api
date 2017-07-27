d648236dc332b2d1fbccc7be93b0e06ce2a7660b
Dhwty <dhwty@localhost.localdomain>
2017-07-27 15:02:04 +0300

    Add secrete key for authentication

--- instance/config.py
+++ instance/config.py
@@ -4,7 +4,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = os.getenv('SECRET')
+    SECRET_KEY = os.getenv('SECRET', 'my_precious')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
 
