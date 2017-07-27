b60a5c208cc011b2e2dc62a5dd6905f8ad7adcc1
Dhwty <dhwty@localhost.localdomain>
2017-07-27 12:10:42 +0300

    Add the authentication

--- app/models/models.py
+++ app/models/models.py
@@ -1,3 +1,6 @@
+import jwt
+import datetime
+
 from app import db
 
 
@@ -30,8 +33,45 @@ class User(db.Model):
         db.session.delete(self)
         db.session.commit()
 
+    def encode_auth_token(self, user_id):
+        """
+        Generates the Auth Token
+        :return: string
+        """
+        try:
+            payload = {
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
+                'iat': datetime.datetime.utcnow(),
+                'sub': user_id
+            }
+            return jwt.encode(
+                payload,
+                app.config.get('SECRET'),
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
+
+
+    @staticmethod
+    def decode_auth_token(auth_token):
+        """
+        Decodes the auth token
+        :param auth_token:
+        :return: integer|string
+        """
+        try:
+            payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'))
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            return 'Signature expired. Please log in again.'
+        except jwt.InvalidTokenError:
+            return 'Invalid token. Please log in again.'
+
+
+
     def __repr__(self):
-        return "<User: {}, with email: {}.>".format(self.name, self.email)
+        return "<User: {}, with email: {}.>".format(self.username, self.email)
 
 
 class Bucketlist(db.Model):
