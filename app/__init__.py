69a4dd67d0c297e887d73d1d02a9429961b1e488
Dhwty <dhwty@localhost.localdomain>
2017-07-27 12:09:49 +0300

    Add delete endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -99,93 +99,38 @@ def create_app(config_name):
         response.status_code = 200
         return response
 
-    # @app.route('/bucketlists/<int:id>', methods=['GET', 'PUT', 'DELETE'])
-    # def bucketlist_manipulation(id, **kwargs):
-    #  # retrieve a buckelist using it's ID
-    #     bucketlist = Bucketlist.query.filter_by(id=id).first()
-    #     if not bucketlist:
-    #         # Raise an HTTPException with a 404 not found status code
-    #         abort(404)
+    @app.route('/bucketlists/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+    def bucketlist_manipulation(id):
+     # retrieve a buckelist using it's ID
+        bucketlist = Bucketlist.query.filter_by(id=id).first()
+        if not bucketlist:
+            # Raise an HTTPException with a 404 not found status code
+            abort(404)
 
-    #     if request.method == 'DELETE':
-    #         bucketlist.delete()
-    #         return {
-    #         "message": "bucketlist {} deleted successfully".format(bucketlist.id) 
-    #      }, 200
+        if request.method == 'DELETE':
+            bucketlist.delete()
+            return {
+            "message": "bucketlist {} deleted successfully".format(bucketlist.id) 
+         }, 200
 
-    #     elif request.method == 'PUT':
-    #         name = str(request.data.get('name', ''))
-    #         bucketlist.name = name
-    #         bucketlist.save()
-    #         response = jsonify({
-    #             'id': bucketlist.id,
-    #             'name': bucketlist.name,
-    #             'date_created': bucketlist.date_created,
-    #             'date_modified': bucketlist.date_modified
-    #         })
-    #         response.status_code = 200
-    #         return response
-    #     else:
-    #         # GET
-    #         response = jsonify({
-    #             'id': bucketlist.id,
-    #             'name': bucketlist.name,
-    #             'date_created': bucketlist.date_created,
-    #             'date_modified': bucketlist.date_modified
-    #         })
-    #         response.status_code = 200
-    #         return response
+        elif request.method == 'PUT':
+            # PUT
+            name = request.data.get('name')
+            bucketlist.name = name
+            bucketlist.save()
+            response = jsonify({
+                'id': bucketlist.id,
+                'name': bucketlist.name
+            })
+            response.status_code = 200
+            return response
+        elif request.method == 'GET':
+            # GET
+            response = jsonify({
+                'id': bucketlist.id,
+                'name': bucketlist.name
+            })
+            response.status_code = 200
+            return response
 
     return app
-
-
-
-
-
-
-
-
-
-
-# app = Flask(__name__)
-# app.config.from_object('config')
-# db = SQLAlchemy(app)
-
-
-# @app.route('/auth/register', methods=['POST'])
-# def register():
-#     return ''
-
-
-# @app.route('/auth/login', methods=['POST'])
-# def login():
-#     return ''
-
-
-# @app.route('/auth/logout', methods=['POST'])
-# def logout():
-#     return ''
-
-# @app.route('/auth/reset_password', methods=['POST'])
-# def reset_password():
-#     return ''
-
-
-# @app.route('/bucketlists', methods=['GET', 'POST'])
-# def bucketlists():
-#     return ''
-
-
-# @app.route('/bucketlists/<id>', methods=['GET', 'PUT', 'DELETE'])
-# def single_bucketlists():
-#     return ''
-
-
-# @app.route('/bucketlists/<id>/items', methods=['POST'])
-# def add_items():
-#     return ''
-
-
-# @app.route('/bucketlists/<id>/items/<items_id>', methods=['PUT', 'DELETE'])
-# def change_items():
-#     return ''
