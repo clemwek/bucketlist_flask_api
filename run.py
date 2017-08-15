b588acdfe626f027b01c707a483ea01a1ef310a5
Dhwty <dhwty@localhost.localdomain>
2017-08-15 11:13:27 +0300

    Fix Port

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from app import create_app
 
 config_name = os.getenv('APP_SETTINGS') # config_name = "development"
 app = create_app(config_name)
+port = int(os.environ.get('PORT', 5000))
 
 if __name__ == '__main__':
-    app.run(port=os.getenv('PORT'))
+    app.run('', port=port)
