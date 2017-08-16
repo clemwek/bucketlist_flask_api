864c948957220ed0e5ed8edf9ae44a222a0c4c5a
Dhwty <dhwty@localhost.localdomain>
2017-08-16 15:40:20 +0300

    Test heroku

--- run.py
+++ run.py
@@ -5,7 +5,8 @@ from app import create_app
 
 config_name = os.getenv('APP_SETTINGS') # config_name = "development"
 app = create_app(config_name)
-port = int(os.environ.get('PORT', 5000))
+
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
     app.run('', port = port)
