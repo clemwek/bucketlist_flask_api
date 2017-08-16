75e87f2cc1348fc1af6235260058adc62d996f0e
Dhwty <dhwty@localhost.localdomain>
2017-08-16 15:33:24 +0300

    Test keroku

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from app import create_app
 
 config_name = os.getenv('APP_SETTINGS') # config_name = "development"
 app = create_app(config_name)
-# port = int(os.environ.get('PORT', 5000))
+port = int(os.environ.get('PORT', 5000))
 
 if __name__ == '__main__':
-    app.run()
+    app.run('', port = port)
