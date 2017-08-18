18c15a0f681e2c4fd2ac10195705deb15ccece1a
dhywty <clemwek@gmail.com>
2017-08-19 01:32:14 +0300

    Fix inconsistency in the messages

--- app/views/user.py
+++ app/views/user.py
@@ -3,7 +3,7 @@ This has all the auth code
 """
 
 
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify
 from app.models.models import User
 
 user_blueprint = Blueprint('auth', __name__)
@@ -15,20 +15,24 @@ def register():
     username = str(request.data.get('username', ''))
     email = str(request.data.get('email', ''))
     password = str(request.data.get('password', ''))
-    found_username = User.query.filter_by(username=username).first()
-    found_email = User.query.filter_by(email=email).first()
+
     if not email or not username or not password:
         res = jsonify({'message': 'Some data is missing!'})
         res.status_code = 406
         return res
+
+    found_username = User.query.filter_by(username=username).first()
+    found_email = User.query.filter_by(email=email).first()
     if found_username:
         res = jsonify({'message': 'Username already used try anotherone.'})
         res.status_code = 409
         return res
+
     if found_email:
         res = jsonify({'message': 'Email already used try anotherone.'})
         res.status_code = 409
         return res
+
     new_user = User(username, email)
     new_user.hash_password(password)
     new_user.save()
@@ -47,24 +51,26 @@ def login():
     """ Login a user """
     username = str(request.data.get('username', ''))
     password = str(request.data.get('password', ''))
+
     if not username or not password:
-        return make_response('could not veryfy: No data was send',
-                             401,
-                             {'WWW-Authenticate':'Basic realm="login required!"'})
+        res = jsonify({'message': 'could not veryfy: Some data was not send'})
+        res.status_code = 403
+        return res
 
     found_user = User.query.filter_by(username=username).first()
     if not found_user:
-        return make_response('could not veryfy: No user',
-                             401,
-                             {'WWW-Authenticate':'Basic realm="login required!"'})
+        res = jsonify({'message': 'could not veryfy: No user'})
+        res.status_code = 401
+        return res
 
     if found_user.check_hashed_password(password, found_user.password_hash):
         res = found_user.gen_token()
         res.status_code = 202
         return res
-    return make_response('could not veryfy: wrong password',
-                         401,
-                         {'WWW-Authenticate':'Basic realm="login required!"'})
+    else:
+        res = jsonify({'message': 'could not veryfy: wrong password'})
+        res.status_code = 401
+        return res
 
 
 @user_blueprint.route('/logout', methods=['POST'])
